0b5b23a7ef2b2a3c276870811a4bac5e29acba16
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-06-27 23:53:40 +0530

    Fix typo, reduced salting rounds

--- Controllers/signup.controller.js
+++ Controllers/signup.controller.js
@@ -6,21 +6,20 @@ const createUser = async ( req , res ) => {
     try{
         const { firstName , lastName , email , password } = req.body ;
         const isUserPresent = await User.find({ email })
-        if( isUserPresent && isUSerPresent.length > 0 ){
+        if( isUserPresent && isUserPresent.length > 0 ){
             return res.status(409).json({ success : false , message : "User already exists!!"});
         }
-        else{
-            const saltRounds = 20;
-            const hashedPassword = bcrypt.hash( password, saltRounds );
-            const savedUser = new User({
-                _id : new mongoose.Types.ObjectId(),
-                firstName,
-                lastName,
-                password : hashedPassword
-            })
-            await savedUser.save(); 
-            return res.status(201).json({ success : true , message : "User created!" });
-        }
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash( password, saltRounds );
+        const savedUser = new User({
+            _id : new mongoose.Types.ObjectId(),
+            firstName,
+            lastName,
+            email,
+            password : hashedPassword
+        })
+        await savedUser.save(); 
+        return res.status(201).json({ success : true , message : "User created!" });
     }catch( err ){
         res.json({ 
             success: false , 
