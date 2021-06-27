4922cd1b17f4a47a4fb0460cc714e6e9f4cf932e
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-06-28 00:01:20 +0530

    Add await with find

--- Controllers/login.controller.js
+++ Controllers/login.controller.js
@@ -6,18 +6,17 @@ const loginUser = async ( req , res ) => {
     try{
         const { email , password } = req.body;
         //search if the user is registered or not
-        const user = User.find({ email })
+        const user = await User.findOne({ email })
         if(!user){
             return res.status(401).json({ success: false , message:"User not found!" });
-        }else{
-            //check if the password match
-            const match = await bcrypt.compare(password, user.password);
-            if(!match){
-                return res.status(401).json({ success : false , message : "Error! Wrong login Credentials" })
-            } 
-            const token = jwt.sign({ userId : user._id } , process.env.SECRET , { expiresIn : '24h' })
-            res.status(200).json({ success : true , message : "Login Successful!" , token })
         }
+        //check if the password match
+        const match = await bcrypt.compare(password, user.password);
+        if(!match){
+            return res.status(401).json({ success : false , message : "Error! Wrong login Credentials" })
+        } 
+        const token = jwt.sign({ userId : user._id } , process.env.SECRET , { expiresIn : '24h' })
+        return res.status(200).json({ success : true , message : "Login Successful!" , token })
     }catch(err){
         res.json({ success: false , message: "Error in logging!" , errorMessage : err.message })
     }
