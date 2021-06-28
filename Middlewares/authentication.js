80c1b90c87acf4800c7248f3bc759da923f3c1eb
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-06-28 13:30:32 +0530

    Add auth verification middleware

---
+++ Middlewares/authentication.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken')
+const { User } = require('../Models/user.model')
+
+const verifyAuth = ( req , res , next ) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        if(!token){
+            return res.status(401).json({ success : false , message : 'Unauthorized access! Token missing' });
+        }
+        const decoded = jwt.verify( token , process.env.SECRET );
+        const isUserIdExists = await User.findOne({ _id : decoded.userId })
+        if( isUserIdExists ){
+            req.user = { userId : decoded.userId }
+            return next()
+        }else{
+            throw Error
+        }
+    }catch(err){
+        return res.status(403).json({ 
+            success : false , 
+            message : 'Unauthorized access! token error' , 
+            errorMessage : err.message 
+        })
+    }
+}
+module.exports = { verifyAuth }
