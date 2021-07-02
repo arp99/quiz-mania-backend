b069a102d0ad58ec18a667e0c0aae28451ea4404
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-07-02 23:21:20 +0530

    Add router for user

---
+++ Routers/user.router.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+const { verifyAuth } = require('../Middlewares/authentication')
+const { getUserData, getAllUserNames } = require('../Controllers/user.controller')
+
+router.get( "/" , verifyAuth , getUserData )
+router.get("/all", getAllUserNames )``
+module.exports = router
