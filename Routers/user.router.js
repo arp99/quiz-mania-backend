80c49047de8df4dfb14e142c6d0cfc351b7dc3a8
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-07-21 18:40:56 +0530

    Add results route
    Add results route to handle requests to save and update quiz results of an user

--- Routers/user.router.js
+++ Routers/user.router.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const { verifyAuth } = require('../Middlewares/authentication')
-const { getUserData, getAllUserNames } = require('../Controllers/user.controller')
+const { getUserData, getAllUserNames, saveQuizResults } = require('../Controllers/user.controller')
 
 router.get( "/" , verifyAuth , getUserData )
 router.get("/all", getAllUserNames )
+router.post("/results" , verifyAuth, saveQuizResults )
 module.exports = router
