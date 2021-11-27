23d0c48ea1372550bcff82160b321016e2d5e156
Arpan Mondal <arpan19991025@gmail.com>
2021-11-27 12:17:55 +0530

    Add route for leaderboard

--- Routers/user.router.js
+++ Routers/user.router.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const { verifyAuth } = require('../Middlewares/authentication')
-const { getUserData, getAllUserNames, saveQuizResults } = require('../Controllers/user.controller')
+const { getUserData, getAllUserNames, saveQuizResults, getLeaderBoardForQuiz } = require('../Controllers/user.controller')
 
 router.get( "/" , verifyAuth , getUserData )
 router.get("/all", getAllUserNames )
 router.post("/results" , verifyAuth, saveQuizResults )
+router.get("/leaderboard", verifyAuth, getLeaderBoardForQuiz )
 module.exports = router
